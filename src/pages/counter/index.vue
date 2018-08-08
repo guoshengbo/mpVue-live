358af13f229a74f1015882631776e1feb362211f
707562656@qq.com <707562656@qq.com>
2018-08-08 17:16:59 +0800

    video

--- src/pages/counter/index.vue
+++ src/pages/counter/index.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="counter-warp">
    <!-- <live-player 
-   src="http://dlhls.cdn.zhanqi.tv/zqlive/197372_1t79D.m3u8" 
+   src="http://hls3.douyucdn.cn/live/288016rlols5_900/playlist.m3u8" 
    mode="live" 
-   autoplay 
-   bindstatechange="statechange" 
+   autoplay="true" 
+   bindstatechange="statechange"
    binderror="error" 
-   style="width: 300px; height: 225px;" />
-   <video id="__video_835660" 
-    class="video-4948d5" 
-    src="https://www.douyu.com/4a4e8fd3-1632-47c4-ad60-75d902e06240">
-  </video> -->
-  <web-view src="http://www.douyu.com/911" />
-  <iframe src="http://www.quanmin.tv/laoyuegou/29105" width="300px" height="200px"></iframe>
+   style="width: 50px; height: 30px;" />  -->
+  <video src="http://hls3.douyucdn.cn/live/288016rlols5_900/playlist.m3u8?wsSecret=ae5141b043211961d5bc1dc564f5fb8a&wsTime=1533719547&token=cpn-androidmpro-0-288016-67e0556a4d274e87dcb8d284b89bb5a8&did=48fef8f4390cfb6b8ad2244e000715q1">
+    <!-- <cover-view class="voice-cover">
+      <cover-image class="voice-img" src="https://cs-op.douyucdn.cn/dyfelocal/act/5acc7c96d51caa4669f8b2de/audio-live-cover.png?timestamp=1530193807"></cover-image>
+    </cover-view> -->
+  </video> 
+  <!-- <div style="width:200px; height:200px"> -->
+  <!-- <web-view src="http://hls3.douyucdn.cn/live/288016rlols5_900/playlist.m3u8?wsSecret=07de3360fa34f3ae686de792b6f8df54&wsTime=1533714549&token=cpn-androidmpro-0-288016-090e323b2ce7a648328ed942c464699c&did=6c790b9b70f89ca00ee864b0000715q1" /> -->
+  <!-- </div> -->
+  <!-- <web-view src="http://www.quanmin.tv/laoyuegou/29105" /> -->
+  <!-- <iframe src="http://www.quanmin.tv/laoyuegou/29105" width="300px" height="200px"></iframe> -->
     <a href="/pages/index/main" class="home">去往首页</a>
   </div>
 </template>
